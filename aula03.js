744d9a253c165166a8f1a9796696f83190ed916e
gustavo pereira <gustavo.rocha103@etec.sp.gov.br>
2024-04-10 08:33:33 -0300

    aula 03 sobre array e objetos

---
+++ aula03.js
@@ -0,0 +1,22 @@
+let n = 2
+let t = "oi"
+
+console.log(typeof n)
+console.log(typeof t)
+
+let a = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+console.log(typeof a)
+
+let o = {nome: "João", idade: "36"}//JSON
+//JavaScript object notation
+
+console.log(typeof o)
+console.log(a)
+console.log(a[6])
+
+a.forEach((el) => 
+    console.log(`Elemento: ${el}`)
+)
+
+console.log(o.nome, "tem ", o.idade)
